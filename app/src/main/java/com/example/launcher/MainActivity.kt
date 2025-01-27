ee04398a72e5220259aa50bb0559944381396f0a
Galen Ferrara <galen.ferrara557@gmail.com>
2025-01-27 13:25:32 +0000

    TODO Added

--- app/src/main/java/com/example/launcher/MainActivity.kt
+++ app/src/main/java/com/example/launcher/MainActivity.kt
@@ -7,6 +7,15 @@ import com.example.launcher.ui.theme.LauncherTheme
 import com.example.launcher.view.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+// TODO: add background customization
+// TODO: add app home/drawer
+// TODO: add popular apps
+// TODO: add widgets
+// TODO: add folders
+// TODO: add screen-time watcher
+// TODO: add screen-time features
+// TODO: add custom settings system
+
 @AndroidEntryPoint // HILT
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
