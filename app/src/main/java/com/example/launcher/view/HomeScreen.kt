4d4fdf465caea12b12119912b1519eebb91a893e
Galen Ferrara <galen.ferrara557@gmail.com>
2025-02-17 16:05:46 +0000

    Added frontend folder design - center aligned folder labels - Added app icons - Formated app "boxes" - Added background to app folders - Apps display in a grid in their folders

--- app/src/main/java/com/example/launcher/view/HomeScreen.kt
+++ app/src/main/java/com/example/launcher/view/HomeScreen.kt
@@ -1,12 +1,17 @@
 package com.example.launcher.view
 
+import android.graphics.drawable.Drawable
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -23,14 +28,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.launcher.viewmodel.HomeViewModel
+import com.google.accompanist.drawablepainter.DrawablePainter
 
 @Composable
 fun HomeScreen(
@@ -42,6 +52,10 @@ fun HomeScreen(
     var showInputDialog by remember { mutableStateOf(false) }
     var newFolderName by remember { mutableStateOf("") }
     val appsInFolder by viewModel.appsInFolder.collectAsState(emptyList())
+
+    val context = LocalContext.current
+    val packageManager = context.packageManager
+
 
     // TODO: Allow multiple folders to be open together
     var expandedFolder by remember { mutableStateOf<String?>(null) } // Track which folder is expanded
@@ -65,20 +79,33 @@ fun HomeScreen(
             .padding(16.dp),
         contentAlignment = Alignment.Center
     ) {
-        Column(
+        Column( // Screen Column
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             folders.forEach { folder ->
-                Column {
-                    // TODO: make this less ugly
-                    // folder
-                    Text(
+
+                // Define the folder modifier dynamically
+                val folderModifier = if (expandedFolder == folder) { // Defines a new modifier for when the folder is opened.
+                    Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(Color.DarkGray.copy(alpha = 0.6F))
+                        .padding(8.dp)
+                } else {
+                    Modifier
+                }
+
+                Column( // Opened folder column
+                    modifier = folderModifier,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text( // Folder Label
                         text = folder,
-                        style = MaterialTheme.typography.headlineMedium,
+                        style = MaterialTheme.typography.displaySmall,
+                        color = Color.White,
                         modifier = Modifier
                             .clip(RoundedCornerShape(8.dp))
-                            .background(Color.LightGray.copy(alpha = 0.8F))
                             .padding(8.dp)
                             .clickable {
                                 if (expandedFolder == folder) {
@@ -90,23 +117,51 @@ fun HomeScreen(
                             }
                     )
 
-                    // TODO: Display apps as app icons and horizontally
                     // display apps when folder is expanded
                     if (expandedFolder == folder) {
-                        Column(
+                        Column( // Column that designates the folder label to appear above the app items
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .background(Color.White.copy(alpha = 0.9F))
-                                .padding(8.dp)
+                                .padding(8.dp),
                         ) {
-                            appsInFolder.forEach { app ->
-                                Text(
-                                    text = app.label,
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    modifier = Modifier
-                                        .padding(8.dp)
-                                        .clickable { viewModel.launchApp(app.packageName) } // Launch app on click
-                                )
+                            LazyVerticalGrid( // Grid designates the locations of the apps inside the folder
+                                columns = GridCells.Fixed(3),
+                            ) {
+                                items(appsInFolder) {app ->
+                                    val icon: Drawable = packageManager.getApplicationIcon(app.packageName)
+
+                                    Box( // Box containing single app item
+                                        modifier = Modifier
+                                            .padding(20.dp)
+                                            .size(64.dp)
+                                    ) {
+                                        Column( // Column designating the icon to be drawn above the label
+                                            modifier = Modifier
+                                                .clickable { viewModel.launchApp(app.packageName) }, // Launch app on click
+                                            horizontalAlignment = Alignment.CenterHorizontally
+                                        )
+                                        {
+                                            Image( // App Icon
+                                                painter = DrawablePainter(icon), // This is the use of the accompanist library, which is shit, just an fyi, took my almost two days to get working
+                                                contentDescription = "${app.label} icon",
+                                                modifier = Modifier
+                                                    .size(40.dp) // Icon Size
+                                                    .padding(5.dp)
+                                                    .align(Alignment.CenterHorizontally)
+                                            )
+                                            Text( // App Label
+                                                text = app.label,
+                                                style = MaterialTheme.typography.titleMedium,
+                                                textAlign = TextAlign.Center,
+                                                maxLines = 1,
+                                                overflow = TextOverflow.Ellipsis,
+                                                color = Color.White,
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                            )
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
